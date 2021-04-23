c332a1c0a94e6c21c53628b4050b875ae57ff49f
QuasarFlash <brownraf21@gmail.com>
2021-04-23 01:37:43 -0400

    hello react

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                I'm Home, Hello Honey
+            </div>
+        )
+    }
+}
+export default Home;
